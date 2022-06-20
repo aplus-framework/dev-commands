8723a7de44bd8e93349c956a28b5e8ae7f28c96e
Natan Felles <natanfelles@gmail.com>
2022-06-19 21:25:10 -0300

    Update seed command

--- src/Seed.php
+++ src/Seed.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /*
  * This file is part of Aplus Framework CLI Commands Library.
  *
@@ -9,17 +9,40 @@
  */
 namespace Framework\CLI\Commands;
 
-use App\Seeds\Seeder;
+use Framework\CLI\CLI;
 use Framework\CLI\Command;
+use Framework\MVC\App;
 
 class Seed extends Command
 {
     protected string $name = 'seed';
-    protected string $description = 'Seed database.';
-    protected string $usage = 'seed';
+    protected string $description = 'Seeds database.';
+    protected string $usage = 'seed [classname]';
+    protected string $databaseInstance = 'default';
 
     public function run() : void
     {
-        (new Seeder(\App::database()))->run();
+        CLI::write(
+            CLI::style('Database Instance:', CLI::FG_YELLOW, formats: [CLI::FM_BOLD])
+            . ' ' . $this->databaseInstance
+        );
+        CLI::newLine();
+        $start = \microtime(true);
+        $this->runSeeder();
+        $end = \microtime(true);
+        CLI::newLine();
+        CLI::write('Total time of ' . \round($end - $start, 6) . ' seconds.');
+    }
+
+    protected function runSeeder() : void
+    {
+        $class = $this->getConsole()->getArgument(0);
+        if (empty($class)) {
+            CLI::error('First argument must be a class name.');
+        }
+        $class = new $class(App::database($this->databaseInstance));
+        $method = new \ReflectionMethod($class, 'runSeed');
+        $method->setAccessible(true);
+        $method->invoke($class, $class);
     }
 }
