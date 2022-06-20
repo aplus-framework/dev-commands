091b758a5e0bfdf570895d7c2de36c894a4ac0d7
Natan Felles <natanfelles@gmail.com>
2022-06-20 18:28:23 -0300

    Add migrateto and migrationversion commands

---
+++ src/MigrateTo.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework CLI Commands Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\CLI\Commands;
+
+use Framework\CLI\CLI;
+
+/**
+ * Class MigrateTo.
+ *
+ * @package cli-commands
+ */
+class MigrateTo extends AbstractMigration
+{
+    protected string $description = 'Runs migrations to a version.';
+
+    public function run() : void
+    {
+        $version = $this->getConsole()->getArgument(0);
+        if ($version === null) {
+            $version = CLI::prompt('Version');
+        }
+        $this->runMigration('to', $version);
+    }
+}
---
+++ src/MigrationVersion.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework CLI Commands Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\CLI\Commands;
+
+use Framework\CLI\CLI;
+use Framework\CLI\Command;
+use Framework\MVC\App;
+
+/**
+ * Class MigrationVersion.
+ *
+ * @package cli-commands
+ */
+class MigrationVersion extends Command
+{
+    protected string $description = 'Shows last migration version name.';
+
+    public function run() : void
+    {
+        $name = App::migrator()->getLastMigrationName();
+        if ($name) {
+            CLI::write($name);
+            return;
+        }
+        CLI::write('No migration.');
+    }
+}
